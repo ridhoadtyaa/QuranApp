248297c2be5d924081309e6156b62a8e00f686ad
ridhoadtyaa <r2603an@gmail.com>
2022-07-05 22:11:55 +0700

    feat: add types for surah detail

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -14,4 +14,40 @@ declare module 'quran-app' {
     Madinah = 'madinah',
     Mekah = 'mekah'
   }
+
+  export interface SuratDetail {
+    status: boolean
+    nomor: number
+    nama: string
+    nama_latin: string
+    jumlah_ayat: number
+    tempat_turun: string
+    arti: string
+    deskripsi: string
+    audio: string
+    ayat: Ayat[]
+    surat_selanjutnya: SuratNextBefore | false
+    surat_sebelumnya: SuratNextBefore | false
+  }
+
+  export interface Ayat {
+    id: number
+    surah: number
+    nomor: number
+    ar: string
+    tr: string
+    idn: string
+  }
+
+  export interface SuratNextBefore {
+    id: number
+    nomor: number
+    nama: string
+    nama_latin: string
+    jumlah_ayat: number
+    tempat_turun: string
+    arti: string
+    deskripsi: string
+    audio: string
+  }
 }
