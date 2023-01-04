ceb240fcdec523f34324ad11747e3ebe5b1aa9df
ridhoadtyaa <r2603an@gmail.com>
2023-01-04 12:43:56 +0700

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV === 'development'
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: { removeConsole: !isDev }
 }
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: isDev
+})
+
+module.exports = withPWA(nextConfig)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,7 @@ class MyDocument extends Document {
     return (
       <Html lang='id'>
         <Head>
+          <link rel='manifest' href='/manifest.json' />
           <link
             rel='icon'
             href='https://ik.imagekit.io/qmw3y9jqe/image_fp2T1MZiW.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1656857628815'
@@ -27,6 +28,13 @@ class MyDocument extends Document {
           <link
             href='https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap'
             rel='stylesheet'
+          />
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+            integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
+            crossOrigin='anonymous'
+            referrerPolicy='no-referrer'
           />
         </Head>
         <body>
